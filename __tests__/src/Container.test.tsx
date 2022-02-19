e1385423ac2c3f0f8049a0786f9723119aa52329
Foo-x <www.sand.x@gmail.com>
2022-02-19 19:05:33 +0900

    feat: hooks to be required

--- __tests__/src/Container.test.tsx
+++ __tests__/src/Container.test.tsx
@@ -2,39 +2,35 @@ import { Container, UseHooks, View } from '@/Container';
 import { render, screen } from '@testing-library/react';
 
 describe('Container', () => {
-  describe('with hooks', () => {
-    type Props = {
-      value: number;
+  type Props = {
+    value: number;
+  };
+
+  type HooksResult = {
+    value: number;
+  };
+
+  const useHooks: UseHooks<Props, HooksResult> = ({ props }) => {
+    return {
+      value: props.value * 2,
     };
+  };
 
-    type HooksResult = {
-      value: number;
-    };
+  const view: View<Props, HooksResult> = ({ props, hooksResult }) => {
+    return (
+      <div>
+        <div data-testid='props-value'>{props.value}</div>
+        <div data-testid='hooks-result-value'>{hooksResult.value}</div>
+      </div>
+    );
+  };
 
-    const useHooks: UseHooks<Props, HooksResult> = ({ props }) => {
-      return {
-        value: props.value * 2,
-      };
-    };
+  const Sut = Container({ useHooks, view });
 
-    const view: View<Props, HooksResult> = ({ props, hooksResult }) => {
-      return (
-        <div>
-          <div data-testid='props-value'>{props.value}</div>
-          <div data-testid='hooks-result-value'>{hooksResult.value}</div>
-        </div>
-      );
-    };
+  test('value', () => {
+    render(<Sut value={10} />);
 
-    const Sut = Container({ useHooks, view });
-
-    test('value', () => {
-      render(<Sut value={10} />);
-
-      expect(screen.getByTestId('props-value')).toHaveTextContent(/^10$/);
-      expect(screen.getByTestId('hooks-result-value')).toHaveTextContent(
-        /^20$/
-      );
-    });
+    expect(screen.getByTestId('props-value')).toHaveTextContent(/^10$/);
+    expect(screen.getByTestId('hooks-result-value')).toHaveTextContent(/^20$/);
   });
 });
--- src/Container.tsx
+++ src/Container.tsx
@@ -1,12 +1,3 @@
-type None = null | undefined;
-type NullObject = Record<string, never> | None | never;
-type MergeIfExists<
-  ToCheck,
-  T extends Record<string, unknown>,
-  MergedKey extends string,
-  MergedValue
-> = [ToCheck] extends [NullObject] ? T : T & { [k in MergedKey]: MergedValue };
-
 export type UseHooksProps<Props> = {
   props: Props;
 };
@@ -14,37 +5,20 @@ export type UseHooks<Props, HooksResult> = (
   useHooksProps: UseHooksProps<Props>
 ) => HooksResult;
 
-export type ViewProps<Props, HooksResult = never> = MergeIfExists<
-  HooksResult,
-  {
-    props: Props;
-  },
-  'hooksResult',
-  HooksResult
->;
-export type View<Props, HooksResult = never> = React.VFC<
-  ViewProps<Props, HooksResult>
->;
+export type ViewProps<Props, HooksResult> = {
+  props: Props;
+  hooksResult: HooksResult;
+};
+export type View<Props, HooksResult> = React.VFC<ViewProps<Props, HooksResult>>;
 
-export type ContainerProps<Props, HooksResult = never> = MergeIfExists<
-  HooksResult,
-  {
-    view: View<Props, HooksResult>;
-  },
-  'useHooks',
-  UseHooks<Props, HooksResult>
->;
-export const Container = <Props, HooksResult = never>(
-  containerProps: ContainerProps<Props, HooksResult>
-) => {
-  const { view } = containerProps;
-  const useHooks = (() => {
-    if ('useHooks' in containerProps) {
-      return containerProps.useHooks;
-    }
-    return () => ({} as HooksResult);
-  })();
-
+export type ContainerProps<Props, HooksResult> = {
+  view: View<Props, HooksResult>;
+  useHooks: UseHooks<Props, HooksResult>;
+};
+export const Container = <Props, HooksResult>({
+  view,
+  useHooks,
+}: ContainerProps<Props, HooksResult>) => {
   const ContainerComponent = (props: Props) => {
     const hooksResult = useHooks({ props });
     const viewProps = {
